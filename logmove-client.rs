74b3e5d941ebf82ad1162033a6e8775ee2a7502e
risonakamo <hmykanae@gmail.com>
2021-03-07 17:35:29 -0500

    testing more post send

--- logmove-client.rs
+++ logmove-client.rs
@@ -1,8 +1,36 @@
+#![allow(non_snake_case)]
+
 use reqwest::Client;
 
 use logmover_remote::types::api_types::{LogMoveRequest,MoveItem};
 
-fn main()
+#[tokio::main]
+async fn main()
+{
+    let testrequest:LogMoveRequest=LogMoveRequest {
+        items:vec![
+            MoveItem {
+                name:"something".to_string(),
+                time:"12asdasfs".to_string()
+            }
+        ]
+    };
+
+    let client=Client::new();
+    let res:String=client.post("http://localhost:4200/log-move")
+        .json(&testrequest)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    println!("{}",res);
+}
+
+#[test]
+fn serialisationTest()
 {
     let testrequest:LogMoveRequest=LogMoveRequest {
         items:vec![
