75750a4a1e26c62c92ead73e916dda96130e16ae
risonakamo <hmykanae@gmail.com>
2021-03-07 17:40:24 -0500

    move log move client into folder split out serialise test

--- logmove-client/logmove-client.rs
+++ logmove-client/logmove-client.rs
@@ -28,25 +28,3 @@ async fn main()
 
     println!("{}",res);
 }
-
-#[test]
-fn serialisationTest()
-{
-    let testrequest:LogMoveRequest=LogMoveRequest {
-        items:vec![
-            MoveItem {
-                name:"something".to_string(),
-                time:"12asdasfs".to_string()
-            }
-        ]
-    };
-
-    println!("{:#?}",&testrequest);
-
-    let serialised:String=serde_json::to_string(&testrequest).unwrap();
-    println!("{}",serialised);
-
-    let deserialised:LogMoveRequest=serde_json::from_str(&serialised).unwrap();
-
-    println!("{:#?}",&deserialised);
-}
---
+++ tests/serialise-test.rs
@@ -0,0 +1,23 @@
+use logmover_remote::types::api_types::{LogMoveRequest,MoveItem};
+
+#[test]
+fn main()
+{
+    let testrequest:LogMoveRequest=LogMoveRequest {
+        items:vec![
+            MoveItem {
+                name:"something".to_string(),
+                time:"12asdasfs".to_string()
+            }
+        ]
+    };
+
+    println!("{:#?}",&testrequest);
+
+    let serialised:String=serde_json::to_string(&testrequest).unwrap();
+    println!("{}",serialised);
+
+    let deserialised:LogMoveRequest=serde_json::from_str(&serialised).unwrap();
+
+    println!("{:#?}",&deserialised);
+}
