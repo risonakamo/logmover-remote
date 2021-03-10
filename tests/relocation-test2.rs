25296bb79014212369fccb8ade980d412a1bd916
risonakamo <hmykanae@gmail.com>
2021-03-10 00:08:16 -0500

    relocate function working

--- tests/relocation-test2.rs
+++ tests/relocation-test2.rs
@@ -1,6 +1,9 @@
-use logmover_remote::relocation2::tests::relocatetest2;
+#![allow(unused_imports)]
+
+use logmover_remote::relocation2::tests::{relocatetest2,relocatetest3};
 
 fn main()
 {
-    relocatetest2();
+    // relocatetest2();
+    relocatetest3();
 }
