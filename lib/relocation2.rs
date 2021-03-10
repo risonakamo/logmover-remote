d4107ca8a49a9f50ce190451e721abf446855b50
risonakamo <hmykanae@gmail.com>
2021-03-09 23:48:19 -0500

    relocate existence function working

--- lib/relocation2.rs
+++ lib/relocation2.rs
@@ -1,10 +1,21 @@
-use super::types::relocation_types::RelocationResult;
+use std::path::Path;
+use colored::Colorize;
+
+use super::types::relocation_types::{RelocationResult,RelocationItemResult};
 
 /// attempt to relocate array of files from the specified target directory to the destination directory.
 /// aborts move attempt if any one of the provided items is missing. returns result.
 fn relocateMultiple(targetDir:&str,destinationDir:&str,items:&Vec<&str>)->RelocationResult
 {
+    let existenceResult:RelocationResult=relocationExistence(targetDir,items);
 
+    if !existenceResult.allSuccess
+    {
+        println!("{}","relocation aborted: unable to find all items".red());
+        return existenceResult;
+    }
+
+    return existenceResult;
 }
 
 /// given a target dir and array of items, check if each item exists. returns a relocation
@@ -12,13 +23,43 @@ fn relocateMultiple(targetDir:&str,destinationDir:&str,items:&Vec<&str>)->Reloca
 /// item individually.
 fn relocationExistence(targetDir:&str,items:&Vec<&str>)->RelocationResult
 {
+    let pathToTarget:&Path=Path::new(targetDir);
 
+    let mut allExist=true;
+    let existResults:Vec<RelocationItemResult>=items.into_iter().map(|x:&&str|->RelocationItemResult {
+        let exists:bool=pathToTarget.join(x).exists();
+
+        if !exists
+        {
+            allExist=false;
+        }
+
+        return RelocationItemResult {
+            name:x.to_string(),
+            success:exists
+        };
+    }).collect();
+
+    return RelocationResult {
+        allSuccess:allExist,
+        itemResults:existResults
+    };
 }
 
 pub mod tests
 {
+    use super::relocationExistence;
+
     pub fn relocatetest2()
     {
+        let result=relocationExistence(
+            r"C:\Users\ktkm\Desktop\logmover-remote\testzone\vids",
+            &vec![
+                "2021-03-07 01:27:36 [Erai-raws] World Witches Hasshin Shimasu! - 07 [1080p][Multiple Subtitle].mkv",
+                "2021-03-06 02:07:55 [Erai-raws] Show by Rock!! Stars!! - 07 [v0][1080p].mkv"
+            ]
+        );
 
+        println!("{:#?}",result);
     }
 }
--- lib/types/relocation_types.rs
+++ lib/types/relocation_types.rs
@@ -1,4 +1,5 @@
 /// result of a relocation attempt
+#[derive(Debug)]
 pub struct RelocationResult
 {
     pub allSuccess:bool, // whether the overall relocation was successful
@@ -6,6 +7,7 @@ pub struct RelocationResult
 }
 
 /// relocation attempt result for a single item
+#[derive(Debug)]
 pub struct RelocationItemResult
 {
     pub name:String,
