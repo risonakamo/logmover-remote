4bb73f827be476592dae7fe673de0c7731bde8e4
risonakamo <hmykanae@gmail.com>
2021-03-27 12:27:03 -0400

    prevent rename overwrite

--- lib/renaming.rs
+++ lib/renaming.rs
@@ -3,20 +3,22 @@ use std::fs::rename;
 use colored::Colorize;
 
 /** rename an item in the target dir. returns bool on if successful. */
-fn renameItem(targetDir:&str,targetItem:&str,newName:&str)->bool
+pub fn renameItem(targetDir:&str,targetItem:&str,newName:&str)->bool
 {
     let pathToFile:PathBuf=Path::new(targetDir).join(targetItem);
     let pathToDestFile:PathBuf=Path::new(targetDir).join(newName);
 
+    if pathToDestFile.exists()
+    {
+        printRenameError(&pathToFile,&pathToDestFile);
+        eprintln!("{}","destination file already exists".red());
+        return false;
+    }
+
     match rename(&pathToFile,&pathToDestFile)
     {
         Err(err)=>{
-            eprintln!("{}","failed rename of file".red());
-            eprintln!("{}",pathToFile.to_str().unwrap().yellow());
-            eprintln!("{} {}",
-                "->".green(),
-                pathToDestFile.to_str().unwrap().yellow()
-            );
+            printRenameError(&pathToFile,&pathToDestFile);
             eprintln!("{}",err);
             return false;
         }
@@ -27,6 +29,17 @@ fn renameItem(targetDir:&str,targetItem:&str,newName:&str)->bool
     return true;
 }
 
+/** print out a failed rename, given the item and new name */
+fn printRenameError(targetItem:&PathBuf,newName:&PathBuf)
+{
+    eprintln!("{}","failed rename of file".red());
+    eprintln!("{}",targetItem.to_str().unwrap().yellow());
+    eprintln!("{} {}",
+        "->".green(),
+        newName.to_str().unwrap().yellow()
+    );
+}
+
 pub mod test
 {
     use super::renameItem;
@@ -35,8 +48,8 @@ pub mod test
     {
         renameItem(
             r"C:\Users\ktkm\Desktop\videos\simulacast\[Nekomoe kissaten&VCB-Studio] Miru Tights [Ma10p_1080p]\SPs",
-            r"[Nekomoe kissaten&VCB-Studio] Miru Tights [Menu][Ma10p_1080p][x265_flac].mkv",
-            "something.mkv"
+            r"[Nekomoe kissaten&VCB-Studio] Miru Tights [PV02][Ma10p_1080p][x265_flac].mkv",
+            "something2.mkv"
         );
     }
 }
