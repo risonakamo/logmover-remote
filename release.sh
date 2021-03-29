4d97ace6f443fe64e70a30689cf4bc03a52bdb47
risonakamo <hmykanae@gmail.com>
2021-03-29 01:43:14 -0400

    include remote rename web in build

--- release.sh
+++ release.sh
@@ -17,10 +17,15 @@ cp example-config/example-client-config.yaml release/client-config.yaml
 cp Rocket.toml release
 cp run-scripts/* release
 
+cd remote-rename-web
+bash release.sh
+cd ..
+cp -r remote-rename-web/release release/remote-rename-web
+
 cd release
 SERVER_ZIP=logmove-server_$VERSION.zip
 CLIENT_ZIP=logmove-client_$VERSION.zip
-7z a -tzip $SERVER_ZIP Rocket.toml logmove-server.exe config.yaml logmove-server.bat
+7z a -tzip $SERVER_ZIP Rocket.toml logmove-server.exe config.yaml logmove-server.bat remote-rename-web
 7z a -tzip $CLIENT_ZIP client-config.yaml logmove-client.exe logmove-client.bat
 mkdir zips
 mv $SERVER_ZIP $CLIENT_ZIP zips
